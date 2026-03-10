36c3f68315988174055b67109eeddbfacdf29e12
Cristian Mayorga <cristiandres1317@gmail.com>
2026-03-10 12:50:02 -0500

    feat: :tada: Estructura

---
+++ backend/src/main/java/com/weatherassistant/weather_outfit_assistant/WeatherOutfitAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.weatherassistant.weather_outfit_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherOutfitAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherOutfitAssistantApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/weatherassistant/weather_outfit_assistant/WeatherOutfitAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherassistant.weather_outfit_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherOutfitAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
